458d3cf8dc73a1875e63e479ed6bc640997627a2
PisitRujirojananon <110370595+PisitRujirojananon@users.noreply.github.com>
2022-08-22 17:41:04 +0700

    Create OccupationController TestCase

--- src/main/java/com/insprie/backendproject/controller/OccupationController.java
+++ src/main/java/com/insprie/backendproject/controller/OccupationController.java
@@ -1,23 +1,21 @@
 package com.insprie.backendproject.controller;
 
-import com.insprie.backendproject.entity.OccupationEntity;
-import com.insprie.backendproject.service.OccupationService;
+import com.insprie.backendproject.model.OccupationEntityList;
+import com.insprie.backendproject.occupation.OccupationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
-public class Occupation {
+public class OccupationController {
 
     @Autowired
     OccupationService occupationService;
 
     @GetMapping("/occupations")
-    public ResponseEntity<List<OccupationEntity>> getOccupations() {
-        List<OccupationEntity> response = occupationService.getOccupations();
+    public ResponseEntity<OccupationEntityList> getOccupations() {
+        OccupationEntityList response = occupationService.getOccupations();
         return ResponseEntity.ok(response);
     }
 }
---
+++ src/test/java/com/insprie/backendproject/occupation/OccupationControllerTest.java
@@ -0,0 +1,53 @@
+package com.insprie.backendproject.occupation;
+
+import com.insprie.backendproject.entity.OccupationEntity;
+import com.insprie.backendproject.model.OccupationEntityList;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class OccupationControllerTest {
+
+    @MockBean
+    OccupationService occupationService;
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    void testOccupationService() {
+
+        OccupationEntityList occupationEntityList = getDataTest();
+        when(occupationService.getOccupations()).thenReturn(occupationEntityList);
+        OccupationEntityList result = testRestTemplate.getForObject("/occupations", OccupationEntityList.class);
+        assertEquals(3, result.getOccupationEntityList().size());
+    }
+
+    public OccupationEntityList getDataTest() {
+        OccupationEntityList occupationEntityList = new OccupationEntityList();
+        List<OccupationEntity> occupationEntities = new ArrayList<>();
+        List<String> occupationName = new ArrayList<>();
+        occupationName.add("NAVY");
+        occupationName.add("OFFICE");
+        occupationName.add("CONSTRUCTION");
+        int couter = 0;
+
+        for (String item : occupationName) {
+            OccupationEntity occupationEntity = new OccupationEntity();
+            occupationEntity.setOccupationtitle(item);
+            occupationEntity.setOccupationid(couter++);
+            occupationEntities.add(occupationEntity);
+        }
+        occupationEntityList.setOccupationEntityList(occupationEntities);
+        return occupationEntityList;
+    }
+}
