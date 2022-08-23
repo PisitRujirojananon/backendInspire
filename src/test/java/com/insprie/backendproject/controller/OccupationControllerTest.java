34c59f32cdc84dd7b583d130e14e1ba08771dacf
Pawabut <kittikawin.pawabut@allianz.com>
2022-08-23 19:43:19 +0700

    Changed gitignore

--- src/test/java/com/insprie/backendproject/controller/OccupationControllerTest.java
+++ src/test/java/com/insprie/backendproject/controller/OccupationControllerTest.java
@@ -1,7 +1,7 @@
-package com.insprie.backendproject.service;
+package com.insprie.backendproject.controller;
 
-import com.insprie.backendproject.controller.OccupationController;
 import com.insprie.backendproject.entity.Occupation;
+import com.insprie.backendproject.service.OccupationService;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,9 +13,11 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @WebMvcTest(OccupationController.class)
 class OccupationControllerTest {
@@ -38,9 +40,9 @@ class OccupationControllerTest {
         mockMvc.perform(MockMvcRequestBuilders
                         .get("/occupations")
                         .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.size()", Matchers.is(3)))
-                .andExpect(jsonPath("$.[2].occupationtitle", Matchers.is("Office Worker")));
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.size()", Matchers.is(3)))
+                    .andExpect(jsonPath("$.[2].occupationtitle", Matchers.is("Office Worker")));
+        }
 
-    }
 }
